f7042663e8e108b17cdbdf3ae0c5589f0bd6177e
Pietro Piva Vieira <pietro.developer@gmail.com>
2022-04-04 06:35:41 -0300

    feat: add Player Schema definition via interface

--- src/data/mongo.ts
+++ src/data/mongo.ts
@@ -1,15 +1,20 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
+mongoose.connect(`mongodb://root:MongoDB2019!@localhost:27017`);
 
-mongoose.connect(`mongodb://root:MongoDB2019!@localhost:27017`)
+interface Player {
+  nickname: string;
+  vocation: string;
+  level: number;
+  createdAt: Date;
+}
 
-const playerSchema = new Schema({
-    nickname:  String, // String is shorthand for {type: String}
-    vocation: String,
-    level: Number,
-    createdAt: Date
-  });
+const playerSchema = new Schema<Player>({
+  nickname: String, // String is shorthand for {type: String}
+  vocation: String,
+  level: Number,
+  createdAt: Date,
+});
 
-
-export const Player = mongoose.model('Player', playerSchema)
+export const Player = mongoose.model('Player', playerSchema);
